fcafd758d7a54eade769d9e9ad78ea4fee8cdf7d
Mark Peters <mapeters101@earthlink.net>
2025-04-06 19:07:43 -0400

    Work on lesson152 from 2:30 to 4:10.

--- lesson152.py
+++ lesson152.py
@@ -3,4 +3,12 @@ print('{1} % {2} = {0}'.format(10%3, 10, 3))
 print('{a} % {b} = {mod}'.format(mod=10%3, a=10, b=3))
 print('-'*80)
 
+a = 10
+b = 3
+print(f'{a} % {b} = {a%b}')
+print(F'{a} % {b} = {a%b}')
+print()
 
+a = 10 / 3
+print(f'{a:0.5f}')
+print(f'{10/3 :0.5f}')
