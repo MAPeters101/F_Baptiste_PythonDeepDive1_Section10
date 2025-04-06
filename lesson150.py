5b549c206618c6ecca5c24e76035bdee0e0b31e8
Mark Peters <mapeters101@earthlink.net>
2025-04-06 18:05:06 -0400

    Complete work on lesson150.

---
+++ lesson150.py
@@ -0,0 +1,22 @@
+print(10000000)
+print(10_000_000)
+print(0xFFFFFF)
+print(0x_FF_FFFF)
+print(0x_F_F_F_F_F_F)
+print(1_0_0_0.54_32_1)
+#print(_10_000)
+#print(10_000_)
+#print(10__000)
+print(10_000)
+print()
+
+print('{:_}'.format(10000))
+print('{:_}'.format(100000000))
+print('{:_X}'.format(16777215))
+print('{:_X}'.format(16777215234765234))
+print()
+
+print(int('FF_FFFF', 16))
+print(int('0b_1_0000_0000', 2))
+
+
